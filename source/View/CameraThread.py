fc745f225d0f11e079f69154a1522eb57e16b185
Kosmas Archontis <up1084020@ac.upatras.gr>
2024-12-12 14:44:35 +0200

    Add timer image functionality to the game

--- source/View/CameraThread.py
+++ source/View/CameraThread.py
@@ -8,6 +8,7 @@ import time
 class CameraThread(QThread):
     frame_ready = pyqtSignal(QImage)  # Signal to send frames to the GUI
     text_ready = pyqtSignal(dict)
+    timer_ready = pyqtSignal(QImage)
 
     def __init__(self, viewController, parent=None):
         super().__init__(parent)
@@ -16,55 +17,11 @@ class CameraThread(QThread):
         self.running = True  # Control flag for the thread
         self.round_delay = 3  # Delay time in seconds
 
-
-    # def run(self):
-    #     try:
-    #         self.viewController.openCamera()
-    #         while self.running:
-    #             ret, frame = self.viewController.updateCameraFrame()
-    #             if ret:
-    #                 # Initiate the game round
-    #                 frame, user_gesture, computer_choice, result = self.viewController.playRound(frame)
-    #                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    #                 self.viewController.handleScore(result)
-    #                 haveWinner, winnerName = self.viewController.checkGameWinner()
-
-    #                 # Collect text information for GUI display
-    #                 text_info = {
-    #                     "User Gesture": user_gesture,
-    #                     "Computer Choice": computer_choice,
-    #                     "Result": result,
-    #                     "User Score": self.viewController.getUserScore(),
-    #                     "Computer Score": self.viewController.getComputerScore(),
-    #                     "Winner": winnerName if haveWinner else None
-    #                 }
-
-    #                 # Emit the text information
-    #                 self.text_ready.emit(text_info)
-
-    #                 # if haveWinner:
-    #                 #     # self.viewController.resetGame()
-    #                 #     self.viewController.showWinner(winnerName)
-
-    #                 # Convert frame to QImage and emit it                    
-    #                 h, w, ch = frame.shape
-    #                 bytes_per_line = ch * w
-    #                 q_image = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
-
-    #                 if q_image.isNull():
-    #                     raise Exception('Error: Invalid QImage object.')
-
-    #                 self.frame_ready.emit(q_image)
-
-    #             time.sleep(0.018)  # ~60 FPS
-
-    #         self.viewController.releaseCamera()
-    #     except Exception as e:
-    #         raise Exception('Error in CameraThread: ' + str(e))
-
     def run(self): 
         try:
             self.viewController.openCamera()
+            # get timer images
+            timer_images = self.viewController.getTimerImages()
             winner_found = False  # Track if a winner has been determined
             round = 0
             while self.running:
@@ -83,19 +40,24 @@ class CameraThread(QThread):
                             raise Exception('Error: Invalid QImage object.')
 
                         self.frame_ready.emit(q_image)
+                    
+                    if not winner_found:
+                        # every integer second, show the image of the timer, e.g. 1, 2, 3
+                        if time.time() - start_time >= 0.5 and time.time() - start_time < 1:
+                            self.timer_ready.emit(QImage(timer_images[2].data, timer_images[0].shape[1], timer_images[0].shape[0], timer_images[0].shape[1] * 3, QImage.Format_RGB888))
+                        elif time.time() - start_time >= 1 and time.time() - start_time < 1.5:
+                            self.timer_ready.emit(QImage(timer_images[1].data, timer_images[0].shape[1], timer_images[0].shape[0], timer_images[0].shape[1] * 3, QImage.Format_RGB888))
+                        elif time.time() - start_time >= 1.5 and time.time() - start_time < 2:
+                            self.timer_ready.emit(QImage(timer_images[0].data, timer_images[0].shape[1], timer_images[0].shape[0], timer_images[0].shape[1] * 3, QImage.Format_RGB888))
+                        elif time.time() - start_time >= 2 and time.time() - start_time < 2.5:
+                            # Go!
+                            pass
 
                     time.sleep(0.018)  # Maintain the frame rate (~60 FPS)
 
                 if winner_found:
                     # If there's already a winner, skip the game logic but keep the feed running
                     continue
-
-                # if winner_found:
-                #     # If there's already a winner, skip the game logic but keep the feed running
-                #     # continue
-                #     winner_found = False
-                #     round = 1
-                #     self.viewController.resetGame()
 
                 # Start a round after the delay
                 ret, frame = self.viewController.updateCameraFrame()
